2e05ec3a71f1844d6dcc4acbe0b08219cd1103e8
jyujyubackend <jyujyubackend@gmail.com>
2023-05-04 00:01:04 +0900

    맛집에 등록된 리뷰 가져오기 API 추가

--- src/main/java/com/jyujyu/review/service/ReviewService.java
+++ src/main/java/com/jyujyu/review/service/ReviewService.java
@@ -3,7 +3,10 @@ package com.jyujyu.review.service;
 import com.jyujyu.review.model.ReviewEntity;
 import com.jyujyu.review.repository.RestaurantRepository;
 import com.jyujyu.review.repository.ReviewRepository;
+import com.jyujyu.review.service.dto.ReviewDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,4 +39,19 @@ public class ReviewService {
         reviewRepository.delete(review);
     }
 
+    public ReviewDto getRestaurantReview(Long restaurantId, Pageable page) {
+        Double avgScore = reviewRepository.getAvgScoreByRestaurantId(restaurantId);
+        Slice<ReviewEntity> reviews = reviewRepository.findSliceByRestaurantId(restaurantId, page);
+
+        return ReviewDto.builder()
+                .avgScore(avgScore)
+                .reviews(reviews.getContent())
+                .page(
+                        ReviewDto.ReviewDtoPage.builder()
+                                .offset(page.getPageNumber() * page.getPageSize())
+                                .limit(page.getPageSize())
+                                .build()
+                )
+                .build();
+    }
 }
